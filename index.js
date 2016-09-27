f6459c239fa390f3d2fdfc3b4157671cd629915e
Simon Wade <simon.wade@gmail.com>
2016-09-27 13:01:03 +1000

    Fix typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var exports = module.exports = createLogger;
 
 function createLogger(name, options) {
   if (!rootLogger) {
-    throw new Error('No root logger - did you forget to call lug.init()');
+    throw new Error('No root logger - did you forget to call lugg.init()');
   }
   if (name) {
     var opts = options || {},
